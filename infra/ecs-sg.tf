c6d81be9a705644457bfcb87e7d5ba2349855bca
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-11-24 14:38:33 +0530

    Create ecs-sg.tf

---
+++ infra/ecs-sg.tf
@@ -0,0 +1,20 @@
+resource "aws_security_group" "ecs_tasks" {
+  name        = "${var.project_name}-ecs-tasks-sg"
+  description = "Security group for ECS Fargate tasks"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "Allow ALB to reach ECS tasks"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
