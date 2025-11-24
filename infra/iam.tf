6f27e6f05a59fc66de182fa27b356c259e9a5889
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-11-24 14:09:23 +0530

    Update iam.tf

--- infra/iam.tf
+++ infra/iam.tf
@@ -1,60 +1,103 @@
 data "aws_iam_policy_document" "codebuild_assume" {
-  statement { actions = ["sts:AssumeRole"] principals { type = "Service" identifiers = ["codebuild.amazonaws.com"] } }
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["codebuild.amazonaws.com"]
+    }
+  }
 }
 
 resource "aws_iam_role" "codebuild" {
-  name = "${var.project_name}-codebuild-role"
+  name               = "${var.project_name}-codebuild-role"
   assume_role_policy = data.aws_iam_policy_document.codebuild_assume.json
 }
 
 data "aws_iam_policy_document" "codebuild_policy" {
   statement {
     actions = [
-      "ecr:GetAuthorizationToken","ecr:InitiateLayerUpload","ecr:UploadLayerPart","ecr:CompleteLayerUpload","ecr:PutImage","ecr:BatchGetImage","ecr:BatchCheckLayerAvailability",
-      "logs:CreateLogGroup","logs:CreateLogStream","logs:PutLogEvents",
+      "ecr:GetAuthorizationToken",
+      "ecr:InitiateLayerUpload",
+      "ecr:UploadLayerPart",
+      "ecr:CompleteLayerUpload",
+      "ecr:PutImage",
+      "ecr:BatchGetImage",
+      "ecr:BatchCheckLayerAvailability",
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
       "s3:*",
-      "ecs:RegisterTaskDefinition","ecs:UpdateService","ecs:DescribeServices","iam:PassRole",
-      "ssm:GetParameters","secretsmanager:GetSecretValue"
+      "ecs:RegisterTaskDefinition",
+      "ecs:UpdateService",
+      "ecs:DescribeServices",
+      "iam:PassRole",
+      "ssm:GetParameters",
+      "secretsmanager:GetSecretValue"
     ]
     resources = ["*"]
   }
 }
 
 resource "aws_iam_role_policy" "codebuild_policy_attach" {
-  name = "${var.project_name}-codebuild-policy"
-  role = aws_iam_role.codebuild.id
+  name   = "${var.project_name}-codebuild-policy"
+  role   = aws_iam_role.codebuild.id
   policy = data.aws_iam_policy_document.codebuild_policy.json
 }
 
 data "aws_iam_policy_document" "codepipeline_assume" {
-  statement { actions = ["sts:AssumeRole"] principals { type = "Service" identifiers = ["codepipeline.amazonaws.com"] } }
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["codepipeline.amazonaws.com"]
+    }
+  }
 }
 
 resource "aws_iam_role" "codepipeline" {
-  name = "${var.project_name}-codepipeline-role"
+  name               = "${var.project_name}-codepipeline-role"
   assume_role_policy = data.aws_iam_policy_document.codepipeline_assume.json
 }
 
 data "aws_iam_policy_document" "codepipeline_policy" {
-  statement { actions = ["codebuild:StartBuild","codebuild:BatchGetBuilds","s3:*","ecs:UpdateService","ecs:DescribeServices","iam:PassRole"] resources = ["*"] }
+  statement {
+    actions = [
+      "codebuild:StartBuild",
+      "codebuild:BatchGetBuilds",
+      "s3:*",
+      "ecs:UpdateService",
+      "ecs:DescribeServices",
+      "iam:PassRole"
+    ]
+    resources = ["*"]
+  }
 }
 
 resource "aws_iam_role_policy" "codepipeline_policy_attach" {
-  name = "${var.project_name}-codepipeline-policy"
-  role = aws_iam_role.codepipeline.id
+  name   = "${var.project_name}-codepipeline-policy"
+  role   = aws_iam_role.codepipeline.id
   policy = data.aws_iam_policy_document.codepipeline_policy.json
 }
 
 data "aws_iam_policy_document" "ecs_task_exec_assume" {
-  statement { actions = ["sts:AssumeRole"] principals { type = "Service" identifiers = ["ecs-tasks.amazonaws.com"] } }
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
 }
 
 resource "aws_iam_role" "ecs_task_exec" {
-  name = "${var.project_name}-ecs-task-exec"
+  name               = "${var.project_name}-ecs-task-exec"
   assume_role_policy = data.aws_iam_policy_document.ecs_task_exec_assume.json
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_exec_attach" {
-  role = aws_iam_role.ecs_task_exec.name
+  role       = aws_iam_role.ecs_task_exec.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
