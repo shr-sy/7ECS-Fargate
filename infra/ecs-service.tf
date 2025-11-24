0ffa50f036a3a254cbd2b0edc7c7cfaad206ed88
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-11-24 14:32:22 +0530

    Create ecs-service.tf

---
+++ infra/ecs-service.tf
@@ -0,0 +1,44 @@
+resource "aws_ecs_task_definition" "main" {
+  family                   = "${var.project_name}-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = 256
+  memory                   = 512
+  execution_role_arn       = aws_iam_role.ecs_task_execution.arn
+  task_role_arn            = aws_iam_role.ecs_task_execution.arn
+
+  container_definitions = jsonencode([
+    {
+      name  = "app"
+      image = "123456789012.dkr.ecr.${var.aws_region}.amazonaws.com/${var.project_name}:latest"
+
+      essential = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+        }
+      ]
+    }
+  ])
+}
+
+resource "aws_ecs_service" "main" {
+  name            = "${var.project_name}-service"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.main.arn
+  launch_type     = "FARGATE"
+  desired_count   = 1
+
+  network_configuration {
+    subnets         = var.private_subnets
+    security_groups = [aws_security_group.ecs_tasks.id]
+    assign_public_ip = false
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.app_tg.arn
+    container_name   = "app"
+    container_port   = 80
+  }
+}
