0ab59ee12ff38766231aa5cd63059cab2fafab9b
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-11-24 14:07:44 +0530

    Update codebuild.tf

--- infra/codebuild.tf
+++ infra/codebuild.tf
@@ -1,27 +1,34 @@
+data "aws_caller_identity" "current" {}
+
 resource "aws_codebuild_project" "build_all" {
-  name          = "${var.project_name}-build-all"
-  description   = "Build all microservices and push images to ECR"
-  service_role  = aws_iam_role.codebuild.arn
+  name         = "${var.project_name}-build-all"
+  description  = "Build all microservices and push images to ECR"
+  service_role = aws_iam_role.codebuild.arn
 
   environment {
-    compute_type                = "BUILD_GENERAL1_MEDIUM"
-    image                       = "aws/codebuild/standard:7.0"
-    type                        = "LINUX_CONTAINER"
-    privileged_mode             = true
+    compute_type    = "BUILD_GENERAL1_MEDIUM"
+    image           = "aws/codebuild/standard:7.0"
+    type            = "LINUX_CONTAINER"
+    privileged_mode = true
+
     environment_variable {
       name  = "AWS_ACCOUNT_ID"
       value = data.aws_caller_identity.current.account_id
     }
-    environment_variable { name = "AWS_DEFAULT_REGION"; value = var.aws_region }
+
+    environment_variable {
+      name  = "AWS_DEFAULT_REGION"
+      value = var.aws_region
+    }
   }
 
   source {
-    type = "GITHUB"
+    type     = "GITHUB"
     location = "https://github.com/${var.github_repo}.git"
     buildspec = file("${path.module}/../buildspec.yml")
   }
 
-  artifacts { type = "NO_ARTIFACTS" }
+  artifacts {
+    type = "NO_ARTIFACTS"
+  }
 }
-
-data "aws_caller_identity" "current" {}
