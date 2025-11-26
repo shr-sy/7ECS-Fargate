d322bd3191f9d6beee700753a1ac2331585fb53d
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-11-26 14:48:49 +0530

    Update outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,3 +1,11 @@
-output "alb_dns" { value = aws_lb.alb.dns_name }
-output "ecr_repos" { value = { for k, r in aws_ecr_repository.services : k => r.repository_url } }
-output "ecs_cluster" { value = aws_ecs_cluster.main.id }
+output "alb_dns" { 
+  value = aws_lb.alb.dns_name 
+}
+
+output "ecr_repos" { 
+  value = { for k, r in aws_ecr_repository.services : k => r.repository_url } 
+}
+
+output "ecs_cluster" { 
+  value = aws_ecs_cluster.main.id 
+}
