8bac4dd174ac201c84cded86c2711aaffab1bb30
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-11-26 12:55:50 +0530

    Update codebuild.tf

--- infra/codebuild.tf
+++ infra/codebuild.tf
@@ -5,10 +5,6 @@ resource "aws_codebuild_project" "build_all" {
   description  = "Build all microservices and push images to ECR"
   service_role = aws_iam_role.codebuild.arn
 
-  artifacts {
-    type = "CODEPIPELINE"
-  }
-
   environment {
     compute_type    = "BUILD_GENERAL1_MEDIUM"
     image           = "aws/codebuild/standard:7.0"
@@ -27,14 +23,11 @@ resource "aws_codebuild_project" "build_all" {
   }
 
   source {
-    type            = "CODEPIPELINE"
-    buildspec       = file("${path.module}/../buildspec.yml")
+    type      = "CODEPIPELINE"
+    buildspec = "buildspec.yml"
   }
 
-  logs_config {
-    cloudwatch_logs {
-      group_name  = "/aws/codebuild/${var.project_name}"
-      stream_name = "build"
-    }
+  artifacts {
+    type = "CODEPIPELINE"
   }
 }
