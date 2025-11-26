e82332464d49eae3b0d9dab0a14c944e6de79cb2
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-11-26 17:46:47 +0530

    Update secrets.tf

--- infra/secrets.tf
+++ infra/secrets.tf
@@ -2,7 +2,7 @@
 # GitHub OAuth Token Secret (USED BY CODEPIPELINE)
 ###############################################
 resource "aws_secretsmanager_secret" "github_oauth_secret" {
-  name = "hcp-ecs-github-token"
+  name = var.github_oauth_token_secret_name
 }
 
 resource "aws_secretsmanager_secret_version" "github_oauth_secret_version" {
