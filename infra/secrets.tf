0f8f758c7ff22ce77aca1944ac19267a008c60a5
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-11-26 16:57:06 +0530

    Update secrets.tf

--- infra/secrets.tf
+++ infra/secrets.tf
@@ -1,29 +1,25 @@
-############################################
 # Store GitHub OAuth Token in Secrets Manager
-############################################
-
 resource "aws_secretsmanager_secret" "github_oauth_secret" {
-  name = var.github_oauth_token_secret_name
+  name = "github-oauth-token"
 }
 
 resource "aws_secretsmanager_secret_version" "github_oauth_secret_version" {
-  secret_id     = aws_secretsmanager_secret.github_oauth_secret.id
+  secret_id = aws_secretsmanager_secret.github_oauth_secret.id
+
   secret_string = jsonencode({
     github_oauth_token = var.github_oauth_token
   })
 }
 
-############################################
-# Store GitHub Webhook Secret in Secrets Manager
-############################################
-
+# GitHub Webhook Secret
 resource "aws_secretsmanager_secret" "github_webhook_secret" {
   name = var.github_webhook_secret_name
 }
 
 resource "aws_secretsmanager_secret_version" "github_webhook_secret_version" {
-  secret_id     = aws_secretsmanager_secret.github_webhook_secret.id
+  secret_id = aws_secretsmanager_secret.github_webhook_secret.id
+
   secret_string = jsonencode({
-    github_webhook_secret = var.github_webhook_secret
+    webhook_secret = "example-webhook-secret"
   })
 }
