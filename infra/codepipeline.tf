1c2cf53252bbefdc5f3eb09e9d1ac6ac8b7ed490
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-11-26 12:57:38 +0530

    Update codepipeline.tf

--- infra/codepipeline.tf
+++ infra/codepipeline.tf
@@ -1,52 +1,16 @@
-#########################################
-# Random ID for S3 Bucket
-#########################################
 resource "random_id" "bucket_id" {
   byte_length = 4
 }
 
-#########################################
-# S3 Bucket for CodePipeline Artifacts
-#########################################
 resource "aws_s3_bucket" "cp_bucket" {
-  bucket        = "${var.project_name}-cp-${random_id.bucket_id.hex}"
-  force_destroy = true
-
-  tags = {
-    Name = "${var.project_name}-cp-bucket"
-  }
+  bucket = lower("${var.project_name}-cp-${random_id.bucket_id.hex}")
 }
 
-# Ownership Controls (required for ACL-disabled buckets)
-resource "aws_s3_bucket_ownership_controls" "cp_bucket_ownership" {
+resource "aws_s3_bucket_acl" "cp_bucket_acl" {
   bucket = aws_s3_bucket.cp_bucket.id
-
-  rule {
-    object_ownership = "BucketOwnerEnforced"
-  }
+  acl    = "private"
 }
 
-# Block public access
-resource "aws_s3_bucket_public_access_block" "cp_bucket_pab" {
-  bucket = aws_s3_bucket.cp_bucket.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
-
-#########################################
-# GitHub CodeStar Connection (Source)
-#########################################
-resource "aws_codestarconnections_connection" "github" {
-  name          = "${var.project_name}-github-connection"
-  provider_type = "GitHub"
-}
-
-#########################################
-# CodePipeline
-#########################################
 resource "aws_codepipeline" "pipeline" {
   name     = "${var.project_name}-pipeline"
   role_arn = aws_iam_role.codepipeline_role.arn
@@ -56,7 +20,6 @@ resource "aws_codepipeline" "pipeline" {
     location = aws_s3_bucket.cp_bucket.id
   }
 
-  # Source Stage
   stage {
     name = "Source"
 
@@ -69,14 +32,13 @@ resource "aws_codepipeline" "pipeline" {
       output_artifacts = ["source_output"]
 
       configuration = {
-        ConnectionArn    = aws_codestarconnections_connection.github.arn
-        FullRepositoryId = var.github_repo
-        BranchName       = "main"
+        ConnectionArn     = aws_codestarconnections_connection.github.arn
+        FullRepositoryId  = var.github_repo
+        BranchName        = "main"
       }
     }
   }
 
-  # Build Stage
   stage {
     name = "Build"
 
@@ -95,7 +57,6 @@ resource "aws_codepipeline" "pipeline" {
     }
   }
 
-  # Deploy Stage
   stage {
     name = "Deploy"
 
@@ -109,7 +70,6 @@ resource "aws_codepipeline" "pipeline" {
 
       configuration = {
         ClusterName = aws_ecs_cluster.main.name
-        ServiceName = aws_ecs_service.main.name
         FileName    = "imagedefinitions.json"
       }
     }
