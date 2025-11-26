ea6bce4c3e69cc606aca8084820ceeb64ff5b0ba
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-11-26 17:39:27 +0530

    Update secrets.tf

--- infra/secrets.tf
+++ infra/secrets.tf
@@ -21,10 +21,3 @@ resource "aws_secretsmanager_secret_version" "github_webhook_secret_version" {
   secret_id     = aws_secretsmanager_secret.github_webhook_secret.id
   secret_string = var.github_webhook_secret
 }
-
-###############################################
-# Data Source for GitHub OAuth Token
-###############################################
-data "aws_secretsmanager_secret_version" "github_token" {
-  secret_id = aws_secretsmanager_secret.github_oauth_secret.id
-}
