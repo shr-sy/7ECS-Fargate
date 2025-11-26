7c8e5e08f77895060844fbf718f6af6f34be64fe
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-11-26 13:24:17 +0530

    Update ecs-sg.tf

--- infra/ecs-sg.tf
+++ infra/ecs-sg.tf
@@ -1,16 +1,21 @@
 resource "aws_security_group" "ecs_tasks" {
   name        = "${var.project_name}-ecs-tasks-sg"
   description = "Security group for ECS Fargate tasks"
-  vpc_id      = aws_vpc.main.id
+  vpc_id      = aws_vpc.this.id
 
-  ingress {
-    description = "Allow ALB to reach ECS tasks"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    security_groups = [aws_security_group.alb_sg.id]
+  # Allow ALB to reach ECS tasks on all service ports
+  dynamic "ingress" {
+    for_each = var.service_ports
+    content {
+      description    = "Allow ALB to reach ${ingress.key} service"
+      from_port       = ingress.value
+      to_port         = ingress.value
+      protocol        = "tcp"
+      security_groups = [aws_security_group.alb_sg.id]
+    }
   }
 
+  # Allow tasks to reach the internet (e.g., DB, external APIs)
   egress {
     from_port   = 0
     to_port     = 0
