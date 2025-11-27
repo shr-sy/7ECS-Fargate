e39166689405be99e0753657d6bc7209ff5fb79f
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-11-27 11:53:04 +0530

    Update providers.tf

--- infra/providers.tf
+++ infra/providers.tf
@@ -2,14 +2,14 @@ provider "aws" {
   region                  = var.aws_region
   skip_metadata_api_check = true
 
-  # Automatically assume the role that Terraform creates
+  # HCP Terraform automatically assumes the IAM role created in iam.tf
   assume_role {
     role_arn = aws_iam_role.terraform_execution_role.arn
   }
 
   default_tags {
     tags = {
-      Project = var.project_name
+      Project     = var.project_name
       Environment = var.environment
     }
   }
