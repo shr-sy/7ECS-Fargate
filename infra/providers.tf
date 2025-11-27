c22c954cfcb803069bf28d6aabde76cd85a5df2e
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-11-27 11:26:37 +0530

    Update providers.tf

--- infra/providers.tf
+++ infra/providers.tf
@@ -1,6 +1,11 @@
 provider "aws" {
-  region = var.aws_region
+  region                  = var.aws_region
   skip_metadata_api_check = true
+
+  # Terraform will assume the execution role automatically
+  assume_role {
+    role_arn = aws_iam_role.terraform_execution_role.arn
+  }
 
   default_tags {
     tags = {
