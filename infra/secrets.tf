a73279315dae3b4628e7620532f576b3faefbd24
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-11-27 11:11:18 +0530

    Update secrets.tf

--- infra/secrets.tf
+++ infra/secrets.tf
@@ -2,7 +2,8 @@
 # GitHub OAuth Token Secret (USED BY CODEPIPELINE)
 ###############################################
 resource "aws_secretsmanager_secret" "github_oauth_secret" {
-  name = var.github_oauth_token_secret_name
+  name                    = var.github_oauth_token_secret_name
+  recovery_window_in_days = 0   # <--- Force delete without recovery
 }
 
 resource "aws_secretsmanager_secret_version" "github_oauth_secret_version" {
@@ -14,7 +15,8 @@ resource "aws_secretsmanager_secret_version" "github_oauth_secret_version" {
 # GitHub Webhook Secret
 ###############################################
 resource "aws_secretsmanager_secret" "github_webhook_secret" {
-  name = var.github_webhook_secret_name
+  name                    = var.github_webhook_secret_name
+  recovery_window_in_days = 0   # <--- Force delete without recovery
 }
 
 resource "aws_secretsmanager_secret_version" "github_webhook_secret_version" {
